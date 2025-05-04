8ad353f2e05a80b87d8d85580c31b41e34220eab
Stanislav Nemchenko <stas.buka@gmail.com>
2025-05-04 21:22:56 +0300

    four task done

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,18 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    // Обчислюємо загальну вартість замовлення
+    const totalPrice = quantity * pricePerDroid;
 
+    // Перевіряємо, чи вистачає коштів у клієнта
+    if (totalPrice > customerCredits) {
+        return "Insufficient funds!";
+    }
+
+    // Якщо коштів вистачає, повертаємо повідомлення про успішне замовлення
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+// Приклад використання
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
